527c1913ff37fbf7bb4ceff29ed4da3f5f2fe06b
HP <HP@HP-PC>
2019-09-24 11:53:07 +0530

    kevin commit

--- GIT project/src/GIT_class.java
+++ GIT project/src/GIT_class.java
@@ -3,7 +3,7 @@ public class GIT_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello. this is kevin rynjah");
+		System.out.println("hello. this is kevin rynja");
 		System.out.println("hello");
 
 	}
